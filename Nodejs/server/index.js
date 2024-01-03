e98d8b363cb4e776a7b3bec4b4000469c083c462
W2-80393 <aasiya.shaikh980@gmail.com>
2024-01-03 14:16:59 +0530

    Nodejs - user & car_details CRUD

--- Nodejs/server/index.js
+++ Nodejs/server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const config = require('config');
 const cors = require('cors');
 const users = require('./routes/users');
+const car_details = require('./routes/car_details');
 
 const PORT = config.get("port");
 
@@ -10,5 +11,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use('/users', users);
+app.use('/cars', car_details);
 
 app.listen(PORT, () => {console.log(`Server started listening at PORT ${PORT}`)});
