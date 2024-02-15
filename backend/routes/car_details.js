c30f4f6ecc6f3beb48521fce65a5c3acbb65de56
AkshatBorana <akshatboranartm@gmail.com>
2024-02-15 22:10:22 +0530

    backend

---
+++ backend/routes/car_details.js
@@ -0,0 +1,156 @@
+const config = require('config');
+const mysql = require('mysql');
+const express = require('express');
+const app = express.Router();
+
+
+
+const connectionDetails = {
+                            host : config.get("server"),
+                            database : config.get("database"),
+                            user : config.get("user"),
+                            password : config.get("password")
+                            }
+
+//to get all car details                            
+app.get('/', (request, response) => {
+    var statement = `select * from car_details`;
+    var connection = mysql.createConnection(connectionDetails);
+
+    connection.query(statement, (error, result) => {
+        if(error==null)
+        {
+            connection.end(); 
+            response.send(result);
+        }
+        else
+        {
+            connection.end();
+            response.send(error);
+        }
+    });
+});
+
+//to get car details with location -- join (if needed) 
+app.get('/cardetails', (request, response) => {
+    var statement = `select cd.model_name, cd.cost_per_hour, cd.transmission, cd.no_of_seats, cd.availability, l.city from car_details cd join location l on cd.location_id = l.location_id;`;
+    var connection = mysql.createConnection(connectionDetails);
+
+    connection.query(statement, (error, result) => {
+        if(error==null)
+        {
+            connection.end(); 
+            response.send(result);
+        } 
+        else
+        {
+            connection.end();
+            response.send(error);
+        }
+    });
+});
+
+
+//to add a car 
+app.post('/', (request, response) => {
+   
+    var model_name = request.body.model_name;
+    var purchase_year = request.body.purchase_year;
+    var availability = request.body.availability;
+    var location_id = request.body.location_id;
+    var no_of_seats = request.body.no_of_seats;
+    var luggage_capacity = request.body.luggage_capacity;
+    var cost_per_hour = request.body.cost_per_hour;
+    var late_fee_per_hour = request.body.late_fee_per_hour;
+    var fuel_type = request.body.fuel_type;
+    var kms_driven = request.body.kms_driven;
+    var transmission = request.body.transmission;
+    var mileage = request.body.mileage;
+    var car_color = request.body.car_color;
+    var user_id = request.body.user_id;
+    var insurance = request.body.insurance;
+    var description = request.body.description;
+
+
+    var statement = `Insert into car_details(model_name, purchase_year, availability, location_id, no_of_seats, luggage_capacity, cost_per_hour, late_fee_per_hour, fuel_type, kms_driven, transmission, mileage, car_color, user_id, insurance, description) 
+                        values('${model_name}','${purchase_year}','${availability}','${location_id}','${no_of_seats}','${luggage_capacity}','${cost_per_hour}','${late_fee_per_hour}','${fuel_type}','${kms_driven}','${transmission}','${mileage}','${car_color}','${user_id}','${insurance}','${description}')`;
+
+    var connection = mysql.createConnection(connectionDetails);
+
+    connection.query(statement, (error, result) => {
+        if(error==null)
+        {
+            connection.end(); 
+            response.send(result);
+        }
+        else
+        {
+            connection.end();
+            response.send(error);
+        }
+    });
+});
+
+
+//to delete a car
+app.delete('/:car_id', (request, response) => {
+
+    var car_id = request.params.car_id;
+
+    var statement = `delete from car_details where car_id='${car_id}'`;
+
+    var connection = mysql.createConnection(connectionDetails);
+
+    connection.query(statement, (error, result) => {
+        if(error==null)
+        {
+            connection.end(); 
+            response.send(result);
+        }
+        else
+        {
+            connection.end();
+            response.send(error);
+        }
+    });
+});
+
+
+//to update car details
+app.put('/:car_id', (request, response) => {
+    
+    var availability = request.body.availability;
+    var cost_per_hour = request.body.cost_per_hour;
+    var late_fee_per_hour = request.body.late_fee_per_hour;
+    var kms_driven = request.body.kms_driven;
+    var car_color = request.body.car_color;
+    var insurance = request.body.insurance;
+    var description = request.body.description;
+
+    var statement = `update car_details set 
+                     availability = '${availability}',
+                     cost_per_hour = '${cost_per_hour}',
+                     late_fee_per_hour = '${late_fee_per_hour}',
+                     kms_driven = '${kms_driven}',
+                     car_color = '${car_color}',
+                     insurance = '${insurance}',
+                     description = '${description}'
+                     where car_id = '${request.params.car_id}'`;
+
+    var connection = mysql.createConnection(connectionDetails);
+
+    connection.query(statement, (error, result) => {
+        if(error==null)
+        {
+            connection.end(); 
+            response.send(result);
+        }
+        else
+        {
+            connection.end();
+            response.send(error);
+        }
+    });
+});
+
+module.exports = app;
---
+++ backend/routes/login.js
@@ -0,0 +1,78 @@
+const mysql = require('mysql');
+const express = require('express');
+const app =  express.Router();
+const atob = require('atob');
+const config = require('config');
+
+//This is for POST /login
+app.post("/",(req, res)=>
+{
+     //GET Username & Password from req.body
+    console.log("data receieved is " + req.body.email+ " " + req.body.password);
+   
+    //Compare credentials with DB
+    var connectionDetails = {
+                                host: config.get("server"),
+                                database: config.get("database"),
+                                user: config.get("user"),
+                                password: config.get("password")
+                            }
+    var connection = mysql.createConnection(connectionDetails);
+    var statement = `select count(*) as count from users where
+                     email='${req.body.email}' and 
+                     password='${req.body.password}'`;
+
+
+    connection.query(statement, (error, result)=>{
+        if(error==null)
+        {
+            console.log("Count Received from DB as " + result[0].count);
+         
+            //Generate Token
+            var token = Math.floor(Math.random() * 10000);
+
+            //Save Token In DB
+            var connection2 = mysql.createConnection(connectionDetails);
+            var statement2 = `update users set token = ${token}
+                              where email='${req.body.email}'`;
+                
+            console.log(statement2);
+            connection2.query(statement2, (error2, result2)=>{
+                if(error2==null)
+                {
+                    if(result2.affectedRows != undefined && 
+                        result2.affectedRows > 0)
+                        {
+                            var reply = {count : result[0].count, 
+                                         token : token};
+                            res.setHeader("Content-Type", 
+                                          "application/json");
+                            res.write(JSON.stringify(reply));
+                            connection2.end();
+                            connection.end();
+                            res.end();
+                        }
+                }
+                else
+                {
+                    res.setHeader("Content-Type", "application/json");
+                    res.write(JSON.stringify(error2));
+                    connection2.end();
+                    res.end();
+                }
+            })
+
+        }
+        else
+        {
+            res.setHeader("Content-Type", "application/json");
+            res.write(JSON.stringify(error));
+            connection.end();
+            res.end();
+        }
+    })
+
+    
+})
+  
+module.exports =app;
